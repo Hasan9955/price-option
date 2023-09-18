3207dde4ea6004756f42c5c563a4ebe533c7c6f1
Hasan9955 <hasanbinali7556@gmail.com>
2023-09-18 14:27:41 +0600

    hasan

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+      <div className="alert alert-info">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+        <span>New software update available.</span>
+      </div>
+
+
+
+      <div className="flex justify-center gap-1 my-1 w-full">
+        <kbd className="kbd">q</kbd>
+        <kbd className="kbd">w</kbd>
+        <kbd className="kbd">e</kbd>
+        <kbd className="kbd">r</kbd>
+        <kbd className="kbd">t</kbd>
+        <kbd className="kbd">y</kbd>
+        <kbd className="kbd">u</kbd>
+        <kbd className="kbd">i</kbd>
+        <kbd className="kbd">o</kbd>
+        <kbd className="kbd">p</kbd>
+      </div>
+      <div className="flex justify-center gap-1 my-1 w-full">
+        <kbd className="kbd">a</kbd>
+        <kbd className="kbd">s</kbd>
+        <kbd className="kbd">d</kbd>
+        <kbd className="kbd">f</kbd>
+        <kbd className="kbd">g</kbd>
+        <kbd className="kbd">h</kbd>
+        <kbd className="kbd">j</kbd>
+        <kbd className="kbd">k</kbd>
+        <kbd className="kbd">l</kbd>
+      </div>
+      <div className="flex justify-center gap-1 my-1 w-full">
+        <kbd className="kbd">z</kbd>
+        <kbd className="kbd">x</kbd>
+        <kbd className="kbd">c</kbd>
+        <kbd className="kbd">v</kbd>
+        <kbd className="kbd">b</kbd>
+        <kbd className="kbd">n</kbd>
+        <kbd className="kbd">m</kbd>
+        <kbd className="kbd">/</kbd>
+      </div>
+
+
+
+
+      <div className="flex justify-center w-full">
+  <kbd className="kbd">▲</kbd>
+</div>
+<div className="flex justify-center gap-12 w-full">
+  <kbd className="kbd">◀︎</kbd>
+  <kbd className="kbd">▶︎</kbd>
+</div>
+<div className="flex justify-center w-full">
+  <kbd className="kbd">▼</kbd>
+</div>
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
